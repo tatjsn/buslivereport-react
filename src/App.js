85c10ed0b8f8ba9048df4b3e47a0376438d652a9
Taz Jackson <tatjsn@users.noreply.github.com>
2020-12-16 00:36:46 +0000

    Add legend and clean up

--- src/App.js
+++ src/App.js
@@ -4,30 +4,40 @@ import {
   YAxis,
   HorizontalGridLines,
   VerticalGridLines,
-  LineMarkSeries
+  LineMarkSeries,
+  DiscreteColorLegend
 } from 'react-vis';
 
 import './App.css';
+
+function lastItem(arr) {
+  return arr[arr.length - 1];
+}
 
 function App({ model }) {
   if (!model) {
     return <div>Now Loading...</div>;
   }
   return (
-    <XYPlot xType="time" width={800} height={600}>
-      <HorizontalGridLines />
-      <VerticalGridLines />
-      <XAxis title="Update" />
-      <YAxis title="Stops Passed" />
-      {
-        Object.keys(model).map(key => (
-          <LineMarkSeries
-            key={key}
-            data={model[key].map(loc => ({ x: loc.last_updated.getTime(), y: loc.stops_passed }))}
-          />
-        ))
-      }
-    </XYPlot>
+    <>
+      <XYPlot xType="time" width={800} height={600}>
+        <HorizontalGridLines />
+        <VerticalGridLines />
+        <XAxis title="Update" />
+        <YAxis title="Stops Passed" />
+        {
+          Object.keys(model).map(key => (
+            <LineMarkSeries
+              key={key}
+              data={model[key].map(loc => ({ x: loc.last_updated.getTime(), y: loc.stops_passed }))}
+            />
+          ))
+        }
+      </XYPlot>
+      <DiscreteColorLegend height={200} width={300} items={Object.keys(model).map(key =>
+        `${key}(${lastItem(model[key]).stops_passed})`
+      )} />
+    </>
   );
 }
 
